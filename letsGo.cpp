04e97e6f2dcbaad1b796bd13a31dc1ca42b53b47
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-20 00:04:54 +0600

    let's go with c++

---
+++ letsGo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Google!\n";
+    cout << "I'm Fahim your new software engineer\n";
+    cout << "I'm very happy to see me here! My Dreams comes true! Alhamdulillah!\n";
+    return 0;
+}
