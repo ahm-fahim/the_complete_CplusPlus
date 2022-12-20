59bb37288d1d1a445d07480e69d6de781529a4f5
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-20 18:30:29 +0600

    constant of c++

---
+++ 02-variables/constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+    cout << minutesPerHour << "\n";
+    cout << PI;
+    return 0;
+}
