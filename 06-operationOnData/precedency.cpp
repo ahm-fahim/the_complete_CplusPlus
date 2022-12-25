b2afe05f28226fc6514c9fe071ee25ccda1d0cb8
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-25 19:08:21 +0600

    prefix and postfix of c++

---
+++ 06-operationOnData/precedency.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a{6}, b{3},c{8},d{9},e{3},f{2},g{5};
+
+    int result = a + b * c - d / e - f + g;
+    cout << "result : " << result << endl;
+
+}
---
+++ prefix_postfix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int value{5};
+    value = value + 1;
+    cout << value << endl;
+
+    value = value - 1;
+    cout << value << endl;
+
+    cout << value++ << endl;
+    cout << ++value << endl;
+    cout << value-- << endl;
+    cout << --value << endl;
+}
