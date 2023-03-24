24892ca91152842d216b92b5f7a4e32f8ad63579
ahm-fahim <contactahmfahim@gmail.com>
2023-03-24 14:57:25 +0600

    c++ setup done successfully

--- 01-syntaxOutputComment/01_letsGo.cpp
+++ 01-syntaxOutputComment/01_letsGo.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Hello Google!\n";
     cout << "I'm Fahim your new software engineer\n";
     cout << "I'm very happy to see me here! My Dreams comes true! Alhamdulillah!\n";
