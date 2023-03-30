30052f489cac317935de3f464c084513e4e2a858
ahm-fahim <contactahmfahim@gmail.com>
2023-03-30 19:40:11 +0600

    Operation Precedency

--- 06-operationOnData/precedency.cpp
+++ 06-operationOnData/precedency.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//Operation Precedency 
+
 int main(){
     int a{6}, b{3},c{8},d{9},e{3},f{2},g{5};
 
     int result = a + b * c - d / e - f + g;
     cout << "result : " << result << endl;
 
+    return 0;
 }
