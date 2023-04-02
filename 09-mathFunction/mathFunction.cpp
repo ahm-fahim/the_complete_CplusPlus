2728b040b5a92a24c7f5a022148e8a15db0b70ee
ahm-fahim <contactahmfahim@gmail.com>
2023-04-02 15:24:15 +0600

    c++ cmath:

--- 09-mathFunction/mathFunction.cpp
+++ 09-mathFunction/mathFunction.cpp
@@ -8,7 +8,7 @@ int main()
 
     double weight{6.7};
     double savings{-5000};
-    double exponential = exp(3); // (1*2*3*4)
+    double exponential = exp(3); // y = a ( 1+ r )^x
 
     cout << "weight rounded to floor is " << floor(weight) << endl;
 
@@ -19,4 +19,15 @@ int main()
     cout << "abs of savings is " << abs(savings) << endl;
 
     cout << "exponential = " << exponential << endl;
+
+    cout << "Power 3^2 = " << pow(3, 2) << endl;
+
+    cout << "Log : to get 54.49 you would elevate e the power of : " << log(54.59) << endl;
+
+    cout << "Log10: to get 1000 you had need to elevate 10 to the power of : " << log10(1000) << endl;
+
+    cout << " The square root of 81 is = " << sqrt(81) << endl;
+
+    cout << "2.4 rounded to " << round(2.4) << endl;
+    cout << "2.6 rounded to " << round(2.6) << endl;
 }
