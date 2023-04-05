e31d025fdd0768a5ff3db4d98770b4821287b540
ahm-fahim <contactahmfahim@gmail.com>
2023-04-06 00:59:45 +0600

    switch statement

---
+++ 011-flowControl_ConditionalProgramming/switch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// TOOLS
+const int Pen{10}, Marker{30}, Eraser{15}, Rectangle{50}, Circle{45}, Ellipse{60};
+
+int main()
+{
+    int tool{Marker};
+
+    switch (tool)
+    {
+    case Pen:
+        cout << "Pen Tool Activated\n";
+        break;
+    case Eraser:
+        cout << "Eraser Tool Activated\n";
+        break;
+    case Marker:
+        cout << "Marker Tool Activated\n";
+        break;
+    case Rectangle:
+        cout << "Rectangle Tool Activated\n";
+        break;
+    case Circle:
+        cout << "Circle Tool Activated\n";
+        break;
+    case Ellipse:
+        cout << "Ellipse Tool Activated\n";
+        break;
+
+    default:
+        cout << "Can't Match Any Tool!\n";
+        break;
+    }
+}
