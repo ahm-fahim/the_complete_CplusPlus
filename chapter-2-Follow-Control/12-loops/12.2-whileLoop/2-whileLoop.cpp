6d2326803efeb3e92777e3c4e9cf2d269160f34e
ahm-fahim <contactahmfahim@gmail.com>
2023-04-09 18:47:04 +0600

    while loop

--- chapter-2-Follow-Control/12-loops/12.2-whileLoop/2-whileLoop.cpp
+++ chapter-2-Follow-Control/12-loops/12.2-whileLoop/2-whileLoop.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
-    std::cout << "what's wrong!" << std::endl;
+    const unsigned int COUNT{10};
+    unsigned int i{};
+
+    while (i < COUNT)
+    {
+        cout << "Count = " << i << endl;
+        cout << "While Loop\n";
+        ++i;
+    }
+
+    return 0;
 }
